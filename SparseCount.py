4796221648e193df4d806c2e720dc95904699c57
Uddipaan <uddipaanh369@gmail.com>
2016-08-20 20:31:29 +0530

    strtinng

---
+++ SparseCount.py
@@ -0,0 +1,22 @@
+__author__ = 'uddipaan'
+
+
+#sparse count algorithm implementation
+
+class SparseCount:
+
+    def __init__(self, prb_sat)
+    
+        self.sat = prb_sat
+    
+    alpha
+    ln = math.log
+    sqrt = math.sqrt
+    delta = 1 - min_conf
+    T = ((ln(1/delta))/alpha)*ln(n)
+
+    i=0
+    while (i <= n) :
+        for t in range(1,T):
+            #hashing to be done
+        if 
