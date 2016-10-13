4dad05185e88bdacacdc346420a4fc7c81896777
Uddipaan <uddipaanh369@gmail.com>
2016-10-13 21:46:49 +0530

    Major codes written i.e, hashfunc/parity constraints and revised

--- extra.py
+++ extra.py
@@ -1,12 +1,79 @@
 __author__ = 'uddipaan'
 
 
-def median(w):
+import random
+
+
+
+class SAT:
+    def __init__(self,prb_nme)
+    
+
+        #here n denotes number of variables
+        file_stream = open(prb_nme)
+        head = file_stream.readline().split()
+        self.n = int(head[2])
+
+        print("There are " + str(self.n) + "variables");
+
+        self.clauses = []
+        self.hashfnc = []
+        self.max_Xor = -1 ''' Set this to non-zero value to limit the maximum length of xor constraints
+                              If this length is exceeded, xor will be broken up into separate clauses'''
+        self.newVar = 0
+
+        while True:
+            curline = ifstream.readline()
+            if not curline:
+                break
+            self.clauses.append(curline.strip())
+        
+
+
+
+
+def median(self, w):
     srt = sorted(w)
     print (srt)
-    leng = len(srt)
-    if not leng % 2:
-        return int((srt[ leng // 2 ] + srt[ leng // 2 - 1]) // 2.0 )
-    return int(srt[ leng // 2 ] )
+    self.leng = len(srt)
+    if not self.leng % 2:
+        return int((srt[ self.leng // 2 ] + srt[ self.leng // 2 - 1]) // 2.0 )
+    return int(srt[ self.leng // 2 ] )
 
 
+
+
+
+
+
+
+def hashfnc_generate(self,m,f)
+    self.hashfnc = []
+    self.newVar = 0
+
+    self.max_Xor = -1
+
+    cur_indx = self.n + 1
+
+    for i in range(0,m):
+        newfnc = []
+
+        for X in range(1, self.n + 1):
+            if random.random() < f:
+                newfnc.append(X)
+            if random.randint(0,1) == 0:
+                newfnc[0] = -newfnc[0]
+                if self.max_Xor > 0:
+                    while len(newfnc) > self.max_Xor:
+                        temp = newfnc[0:self.max_Xor - 1]
+                        newfnc = [cur_indx] + newfnc[self.max_Xor - 1:]
+                        temp.append(cur_indx)
+                        cur_indx += 1
+                        self.newVar += 1
+                        self.hashfnc.append(temp)
+                self.hashfnc.append(newfnc)
+
+    print("Generated " + str(m) + "XOR constraints")
+    if self.max_Xor > 0:
+        print("Max Xor length is " + str(self.max_Xor) + ". Added " + str(self.newVar)+ "new variables!")
+        
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 __author__ = 'uddipaan'
 
 
-from extra import median 
+from extra import * 
 
 #Usage
 #parameter 1: Name of the problem file
 #parameter 2: Alpha value
+#parameter 3: Minimum confidence
 
 if __name__ == '__main__':
     if len(sys.argv)<1:
@@ -14,29 +15,44 @@ if __name__ == '__main__':
 
     prb_nme = sys.argv[1]
     alpha = sys.argv[2]
+    min_conf = sys.argv[3]
+
+    prb = SAT(prb_nme)
+
+    min_m = 0 #no. of  minimum parity constraints is 0
+    if max_m < 0:
+        max_m = prb.n #max no. of parity constraints is the no. of variables in the problem
+
 
     
+
     
     ln = math.log
     sqrt = math.sqrt
     ceiling = math.ceil
     delta = 1 - min_conf
-    Q = (((ln(1/delta))/alpha)*ln(n))
+    Q = (((ln(1/delta))/alpha)*ln(prb.n))
     T = ceiling(Q)
     
-    w = [ for x in range(T)]
+    w = [ for x in range(T)] #here w is initialized to the range of T
     
     i=0
-    while (i <= n) :
+    while (i <= prb.n) :   
         for t in range(1,T):
             
-            #hashing to be done
+            
+            m=i
+            f=0.02 #just for now
+            hashfnc_generate(m, f)  #hash functions generated
+            
+            
+            
             #find S(h)= size of x for which h(x)=0, hash is 0
 
             if sh > = 1:           
-                w[t] = 1
+                w.append(1)
             else
-                w[t] = 0
+                w.append(0)
 
         med = median(w)
         if med < 1:
